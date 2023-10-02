32c52ee1e2756d5f73beb101f4715ca37fec8969
Deborah Okeke <okekedeborah@gmail.com>
2023-10-02 13:09:25 +0100

    create function to reply a comment on a post and to update the respective arrays in the user and comment documents

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -11,8 +11,7 @@ async function createPostComment (req, res) {
 
     // get post_id from the params provided in the request
     const post_id = req.params.post_id
-    console.log(post_id);
-    console.log(req.params);
+    // console.log(post_id);
 
     // destructure comment details from request body
     const {body} = req.body
@@ -28,19 +27,14 @@ async function createPostComment (req, res) {
 
     try {
       // find the id of the user who commented and update the comment_ids array from the userSchema with the new comment id
-      const userUpdateResult = await UserModel.findByIdAndUpdate(user_id, {$push: {comment_ids: commentToSave._id}}, {new: true})
+      await UserModel.findByIdAndUpdate(user_id, {$push: {comment_ids: commentToSave._id}}, {new: true})
   
       // find the id of the post that was commented on and update the comment_ids array from the postSchema with the new comment id
-      const postUpdateResult = await PostModel.findByIdAndUpdate(post_id, {$push: {comment_ids: commentToSave._id}}, {new: true} )
-
-      console.log(userUpdateResult);
-      console.log(postUpdateResult);
+      await PostModel.findByIdAndUpdate(post_id, {$push: {comment_ids: commentToSave._id}}, {new: true} )
       
     } catch (error) {
       console.log(error.message);
     }
-    console.log(post_id);
-    console.log(req.params);
 
     // send status message
     res.status(201).json({success: true, message: 'Comment created successfully', comment: commentToSave})
@@ -53,7 +47,43 @@ async function createPostComment (req, res) {
 
 // function to reply a comment
 async function createCommentReply (req, res) {
+  try {
+    // get user_id from the user property of the request.session object in auth middleware
+    const user_id = req.session.user.id
 
+    // get parent_comment_id from the params provided in the request
+    const parent_comment_id = req.params.comment_id
+
+    console.log('parent comment id:', parent_comment_id);
+    console.log('req params:', req.params);
+
+    // destructure comment (reply) details from request body
+    const {body} = req.body
+
+    // create new comment (reply) and save to database
+    const newReply = new CommentModel({body, user_id, parent_comment_id})
+    const replyToSave = await newReply.save()
+
+    try {
+      // find the id of the user who replied the comment and update the comment_ids array from the userSchema with the new comment id
+      await UserModel.findByIdAndUpdate(user_id, {$push: {comment_ids: replyToSave._id}}, {new: true})
+
+      // find the id of the comment that was replied and update the comment_ids array from the commentSchema with the new comment id
+      const commentToUpdate = await CommentModel.findByIdAndUpdate(parent_comment_id, {$push: {comment_ids: replyToSave._id}}, {new: true})
+
+      console.log('Comment to be updated:', commentToUpdate);
+
+    } catch (error) {
+      console.log(error.message);
+    }
+
+    // send status message
+    res.status(201).json({success: true, message: 'Comment created successfully', comment: replyToSave})
+    
+  } catch (error) {
+    console.log(error.message)
+    res.status(500).json({success: false, message: 'Internal server error'})
+  }
 }
 
 module.exports = {createPostComment, createCommentReply}
