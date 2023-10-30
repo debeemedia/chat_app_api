858a62943b6958a6386832b34420bf23115f6541
Deborah Okeke <okekedeborah@gmail.com>
2023-10-30 17:23:32 +0100

    add cloudinary cloud name and cloudinary api secret

--- utils/imageUpload.js
+++ utils/imageUpload.js
@@ -6,7 +6,9 @@ const {CloudinaryStorage} = require('multer-storage-cloudinary')
 // configure cloudinary
 cloudinary.config({
     secure: true,
-    api_key: process.env.CLOUDINARY_API_KEY
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
 })
 
 // configure the storage engine
