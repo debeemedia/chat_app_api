ced86c98bf35b373b8bae953b24025d13742c91a
Deborah Okeke <okekedeborah@gmail.com>
2023-11-01 07:12:57 +0100

    allow user update profile picture; change transformation from 200 to 500px

--- utils/imageUpload.js
+++ utils/imageUpload.js
@@ -17,7 +17,7 @@ const storage = new CloudinaryStorage({
     params: {
         folder: 'debeechat/profile-pictures',
         // allowed_formats: ['jpg', 'jpeg', 'png'],
-        transformation: [{width: 200, height: 200, crop: 'limit'}]
+        transformation: [{width: 500, height: 500, crop: 'limit'}]
     }
 })
 
